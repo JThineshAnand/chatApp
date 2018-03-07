9d6961aeda772def9736e90b400682b911d2f879
jthineshanand <jthineshanand@gmail.com>
2018-03-07 10:27:56 +0530

    Greet and Alert

--- server/server.js
+++ server/server.js
@@ -1,20 +1,37 @@
 const express = require('express');
 const path = require('path');
-const socketio =require('socket.io');
+const socketIO =require('socket.io');
 const http = require('http');
 
 var app = express();
 app.use(express.static(path.join(__dirname,'../public')));
 var port = process.env.PORT||3000;
 var server = http.createServer(app);
-var io = socketio(server);
+var io = socketIO(server);
 
 io.on('connection',(socket)=>{
     console.log('New User Connected');
+
+    socket.emit('newMessage',{
+      from:'Admin',
+      text:'Welcome to the Chat App',
+      createdAt:new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage',{
+      from:'Admin',
+      text:'New User Joined',
+      createdAt:new Date().getTime()
+    });
+
     socket.on('createMessage',(clientMessage)=>{
       console.log('Message',clientMessage);
+        io.emit('newMessage',{
+          from:clientMessage.from,
+          text:clientMessage.text,
+          createdAt:new Date().getTime()
+        });
     });
-    socket.emit('newMessage',{from:'Server',text:'Hiii'});
 
     socket.on('disconnect',()=>{
       console.log('User Disconnected');
